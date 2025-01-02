e331d1e0808ea1bbebf78016382c80aafec0393c
Victoria Argañaras <varganaras@cuentamono.com>
2025-01-02 16:52:06 -0300

    Add new functions

--- metex/lib/worker.ex
+++ metex/lib/worker.ex
@@ -14,6 +14,18 @@ defmodule Metex.Worker do
     GenServer.call(pid, {:location, location})
   end
 
+  def get_stats(pid) do
+    GenServer.call(pid, :get_stats)
+  end
+
+  def reset_stats(pid) do
+    GenServer.cast(pid, :reset_stats)
+  end
+
+  def stop(pid) do
+    GenServer.cast(pid, :stop)
+  end
+
   ## Server API
   def handle_call({:location, location}, _from, stats) do
     case temperature_of(location) do
@@ -24,6 +36,23 @@ defmodule Metex.Worker do
       _ ->
         {:reply, :error, stats}
     end
+  end
+
+  def handle_call(:get_stats, _from, stats) do
+    {:reply, stats, stats}
+  end
+
+  def handle_cast(:reset_stats, _stats) do
+    {:noreply, %{}}
+  end
+
+  def handle_cast(:stop, stats) do
+    {:stop, :normal, stats}
+  end
+
+  def handle_info(msg, stats) do
+    IO.puts("recaived #{inspecy(msg)}")
+    {:noreply, stats}
   end
 
   ## Helper Functions
@@ -37,7 +66,9 @@ defmodule Metex.Worker do
   end
 
   defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
-    body |> Jason.decode!() |> compute_temperature
+    body
+    |> Jason.decode!()
+    |> compute_temperature
   end
 
   defp parse_response(_) do
