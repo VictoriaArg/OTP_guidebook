b795d0350f8d524855442ddf069c44981943c9dd
Victoria Argañaras <varganaras@cuentamono.com>
2025-03-17 20:58:59 -0300

    demo

--- pooly/lib/sample_worker.ex
+++ pooly/lib/sample_worker.ex
@@ -1,5 +1,6 @@
 defmodule SampleWorker do
   use GenServer
+  require Logger
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, :ok, [])
@@ -14,7 +15,6 @@ defmodule SampleWorker do
   end
 
   def handle_call(:stop, _from, state) do
-    IO.inspect("STOP")
     {:stop, :normal, :ok, state}
   end
 
@@ -24,13 +24,32 @@ defmodule SampleWorker do
     {:stop, :normal, state}
   end
 
+  def handle_cast({:greet_team, nil}, state) do
+    Logger.info("Have a nice weekend")
+    {:noreply, state}
+  end
+
+  def handle_cast({:greet_team, team_member}, state) do
+    Logger.info("Hello #{team_member}")
+    {:noreply, state}
+  end
+
   def work_for(pid, duration) do
     GenServer.cast(pid, {:work_for, duration})
+  end
+
+  def greet_team(pid, []), do: GenServer.cast(pid, {:greet_team, nil})
+
+  def greet_team(pid, [team_member | rest]) do
+    GenServer.cast(pid, {:greet_team, team_member})
+    greet_team(pid, rest)
   end
 end
 
 # w1 = Pooly.checkout "Pool1"
+# Pooly.status "Pool1"
 # w2 = Pooly.checkout "Pool1"
 # w3 = Pooly.checkout "Pool1"
 # SampleWorker.work_for w1, 10_000
 # Pooly.checkout "Pool1", true, :infinity
+# SampleWorker.greet_team w1, ["Vicky", "Male", "Ale S.", "Ale M.", "Ale R.", "Dani", "Carlota"]
